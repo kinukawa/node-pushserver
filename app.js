f27102f6c71b97c83a89a6abb2e2d8add5f0bf0b
Kenji Kinukawa <k.kinukawa@gmail.com>
2013-05-02 14:57:58 +0900

    wsのコード追加

--- app.js
+++ app.js
@@ -33,3 +33,36 @@ app.get('/users', user.list);
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+/**
+ * websocket
+ */
+var webSocketServer = require('ws').Server;
+var wss = new WebSocketServer({port: 3001});
+var connections = [];
+wss.on('connection', function(ws) {
+  connections.push(ws);
+
+  //切断時
+  ws.on('close', function () {
+    console.log('close');
+    connections = connections.filter(function (conn, i) {
+      return (conn === ws) ? false : true;
+    });
+  });
+
+  ws.on('message', function(message) {
+    try {
+      var json = JSON.parse(message);
+      console.log('received: %s,%s', json.message,json.name);
+      for(var i = 0; i < connections.length; i++){
+        var target_ws = connections[i];
+        var reply = {message:json.message,name:json.name};
+        target_ws.send(JSON.stringify(reply));
+      }
+    } catch (e) {
+      console.log(e);
+      return;
+    }
+  });
+});
