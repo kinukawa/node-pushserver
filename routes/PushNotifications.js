079d68850c79a6c8acbefc361310b159efa0bf23
Kenji Kinukawa <k.kinukawa@gmail.com>
2013-05-16 15:54:23 +0900

    wsが配列にないときのエラーハンドリング

--- routes/PushNotifications.js
+++ routes/PushNotifications.js
@@ -6,7 +6,13 @@ var SocketManager = require('../SocketManager');
 exports.create = function(req, res){
   var user_id = req.body.user_id;
   var data = req.body.data;
+
   var wsArr = SocketManager.getWebSockets(user_id);
+  if (!wsArr){
+    res.send("ok");
+    return;
+  }
+
   for (var i = 0; i < wsArr.length; i++){
     var ws = wsArr[i];
     ws.send(data);
