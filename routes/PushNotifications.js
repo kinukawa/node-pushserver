ac19a57f28ff627979cba5d38dcd3e6c9ac0ce81
Kenji Kinukawa <k.kinukawa@gmail.com>
2013-05-16 16:22:56 +0900

    データを文字列に変換してpush

--- routes/PushNotifications.js
+++ routes/PushNotifications.js
@@ -7,15 +7,18 @@ exports.create = function(req, res){
   var user_id = req.body.user_id;
   var data = req.body.data;
 
-  var wsArr = SocketManager.getWebSockets(user_id);
-  if (!wsArr){
-    res.send("ok");
-    return;
-  }
+  for (var i = 0; i < user_id.length; i++){
+    var wsArr = SocketManager.getWebSockets(user_id[i].event_participant.user_id);
+    if (!wsArr){
+      res.send("ok");
+      continue;
+    }
 
-  for (var i = 0; i < wsArr.length; i++){
-    var ws = wsArr[i];
-    ws.send(data);
+    for (var j = 0; j < wsArr.length; j++){
+      var ws = wsArr[j];
+      console.log(ws);
+      ws.send(JSON.stringify(data));
+    }
   }
   res.send("ok");
 };
