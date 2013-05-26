718446f13ee37e502c49b46666667a5a7edc97b0
Kenji Kinukawa <k.kinukawa@gmail.com>
2013-05-26 12:04:01 +0900

    railsのAPIエンドポイントを修正

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 exports.express_port = 2999;
 exports.ws_port = 2998;
 exports.rails_port = 3000;
-exports.rails_host = 'localhost';
-exports.confirm_path = '/api/sessions/confirm.json';
+exports.rails_host = 'api.plannah.dev';
+exports.confirm_path = '/v1/sessions/confirm.json';
